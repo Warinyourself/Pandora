10087ed7d8c320136b174bc6a024e840d1272590
Warinyourself <dmitriysuvorov1997@gmail.com>
2020-08-19 00:00:08 +0300

    Simplify commands and settings page

--- src/views/Commands.vue
+++ src/views/Commands.vue
@@ -5,14 +5,11 @@
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-
-import { ThemeModule } from '@/store/theme'
+import { Component, Vue } from 'vue-property-decorator'
+import { PaletteModule } from '@/store/palette'
 
 @Component
 export default class CommandsPage extends Vue {
-  get isDark() { return ThemeModule.isDark }
-
-  get theme() { return ThemeModule.theme }
+  get isDark() { return PaletteModule.isDark }
 }
 </script>
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -1,20 +1,12 @@
 <template>
   <AppContent>
       <v-btn color="bg">Seetings Here</v-btn>
-      <p> Theme: {{ theme }} </p>
-      <p> isDark: {{ isDark }} </p>
   </AppContent>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
-import { ThemeModule } from '@/store/theme'
-
 @Component
-export default class IndexPage extends Vue {
-  get isDark() { return ThemeModule.isDark }
-
-  get theme() { return ThemeModule.theme }
-}
+export default class IndexPage extends Vue {}
 </script>
