0fb49c69a6192dda95575e90ac4cf95be84f5dc9
Warinyourself <dmitriysuvorov1997@gmail.com>
2020-08-19 23:19:01 +0300

    Create modify color component

---
+++ src/components/page/palette/ModifyColor.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-row>
+    <v-col>
+      <v-color-picker
+        class="ma-2"
+        v-model="value.hex"
+        dot-size="10"
+        width="400px"
+      ></v-color-picker>
+        </v-col>
+        <v-col>
+      <v-select
+        :items="['primary', 'secondary', 'tertiary', 'fg', 'bg' ]"
+        v-model="value.name"
+        label="Set color type"
+      >
+      </v-select>
+    </v-col>
+  </v-row>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+// eslint-disable-next-line no-unused-vars
+import { Palette, PaletteColor } from '@/models/palette'
+
+@Component
+export default class LayoutModifyPalette extends Vue {
+  @Prop({ type: Object, default: () => ({}) }) value!: PaletteColor
+}
+</script>
