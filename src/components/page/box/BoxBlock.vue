83ad09fda65eba0653a2b05886295b35f621c86f
Warinyourself <dmitriysuvorov1997@gmail.com>
2020-08-24 22:32:31 +0300

    Add box block component

---
+++ src/components/page/box/BoxBlock.vue
@@ -0,0 +1,37 @@
+<template>
+  <router-link
+    :to="{ name: 'BoxEdit', params: { id: box.name }}"
+    class="block box-block"
+    :style="`background-image: url(${image.src})`"
+  > 
+    <h2 class="title">  {{ box.name }} </h2>
+  </router-link>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+// eslint-disable-next-line no-unused-vars
+import { Box } from '@/models/box'
+
+@Component
+export default class BoxBlock extends Vue {
+  @Prop({ type: Object, required: true }) box!: Box
+
+  get image() {
+    return this.box.files.find(({ type }) => type === 'image')
+  }
+}
+</script>
+
+<style lang="stylus">
+.box-block
+  min-height 150px
+  cursor pointer
+  color white
+  background-color rgba(0, 0, 0, .1)
+  background-blend-mode darken
+  background-size cover
+  background-repeat no-repeat
+  background-position center
+</style>
