9f18a5f9a03121a95f1585f26175b978c6792c1c
Warinyourself <dmitriysuvorov1997@gmail.com>
2020-08-29 20:16:38 +0300

    Add AppLoader component

---
+++ src/components/app/AppLoader.vue
@@ -0,0 +1,40 @@
+<template>
+  <transition-group
+    tag="div"
+    :name="name"
+  >
+    <v-container
+      v-if="isLoading"
+      key="block-loading"
+      class="block-loading"
+    >
+      <div
+        class="text-center py-7"
+      >
+        <v-progress-circular
+          indeterminate
+          color="primary"
+        />
+      </div>
+    </v-container>
+    <div
+      v-else
+      key="block-content"
+      class="block-content"
+    >
+      <slot
+        v-bind="$attrs"
+      />
+    </div>
+  </transition-group>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator'
+
+@Component
+export default class AppLoader extends Vue {
+  @Prop({ required: true, type: Boolean }) isLoading!: boolean
+  @Prop({ default: 'content-animation', type: String }) name!: String
+}
+</script>
