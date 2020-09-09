4fb50feca7ba5a087975d57747c8ab1a96ebf776
Warinyourself <dmitriysuvorov1997@gmail.com>
2020-09-09 23:44:34 +0300

    Add loader component test on settings page

--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -3,9 +3,9 @@
     <v-btn color="bg">
       Seetings Here
     </v-btn>
-    <AppBlock>
-      <v-row>
-        <v-col cols="auto">
+    <v-row>
+      <v-col cols="auto">
+        <AppBlock class="w-100 ma-0">
           <v-btn
             fab
             dark
@@ -20,9 +20,43 @@
               v-text="'mdi-bell'"
             />
           </v-btn>
-        </v-col>
-      </v-row>
-    </AppBlock>
+          <v-btn
+            fab
+            dark
+            small
+            color="error"
+            class="mt-2 ml-2"
+            @click="startReload"
+          >
+            <v-icon
+              dark
+              color="white"
+              v-text="'mdi-reload'"
+            />
+          </v-btn>
+        </AppBlock>
+      </v-col>
+      <v-col cols="4">
+        <AppBlock class="w-100 ma-0">
+          <AppLoader :is-loading="isLoading">
+            <v-btn
+              fab
+              dark
+              small
+              color="error"
+              class="mt-2"
+              @click="sendErrorNotify"
+            >
+              <v-icon
+                dark
+                color="white"
+                v-text="'mdi-bell'"
+              />
+            </v-btn>
+          </AppLoader>
+        </AppBlock>
+      </v-col>
+    </v-row>
   </AppContent>
 </template>
 
@@ -31,6 +65,15 @@ import { Component, Vue } from 'vue-property-decorator'
 
 @Component
 export default class IndexPage extends Vue {
+  isLoading = true
+
+  startReload() {
+    this.isLoading = true
+    setTimeout(() => {
+      this.isLoading = false
+    }, 1500)
+  }
+
   sendErrorNotify() {
     this.$alert({
       type: 'error',
