fc7430ebcc1abbfe388f0068830a52c1f3ec9e39
Warinyourself <dmitriysuvorov1997@gmail.com>
2020-09-13 23:44:11 +0300

    Generate lines in text editor

--- src/components/app/AppTextEditor.ts
+++ src/components/app/AppTextEditor.ts
@@ -13,9 +13,33 @@ export default class AppTextReader extends Vue {
       h('div', {
         class: 'text-editor-header'
       }, [this.file.name]),
+      this.generateBody(h)
+    ])
+  }
+
+  generateBody(h: CreateElement) {
+    const lines = this.file.text?.split('\n')
+
+    return h('div', {
+      class: 'text-editor-body'
+    }, [
+      lines?.map((text, index) => this.generateLine(h, { text, index }))
+    ])
+  }
+
+  generateLine(h: CreateElement, { index, text }: {index: number; text: string}) {
+    return h('div', {
+      class: 'line'
+    }, [
       h('div', {
-        class: 'text-editor-body'
-      }, [this.file.text])
+        class: 'line-margin'
+      }, index + ''),
+      h('div', {
+        class: 'line-body',
+        domProps: {
+          innerHTML: text
+        }
+      })
     ])
   }
 }
